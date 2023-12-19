a73917fd7309e95f1ef6c2fc1d9c8ab0bbd0ca1d
khuchuan <khuchuan@gmail.com>
2023-12-19 23:26:44 +0700

    Add project files.

---
+++ CodeFirstNewDatabaseSample/BloggingContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstNewDatabaseSample
+{
+    public class BloggingContext: DbContext
+    {
+        public BloggingContext() : base("server=.; database=BlogDB; Integrated Security=true;encrypt=false")
+        {
+            
+        }
+
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
